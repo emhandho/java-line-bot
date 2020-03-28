950d632a9f3f2bc803e4d93f5fb4326911ced3dd
MhNurmanRidho <nurmanrd@gmail.com>
2020-03-28 10:04:31 +0700

    implementasi reply message

--- src/main/java/com/dicoding/linebotjava/demo/Controller.java
+++ src/main/java/com/dicoding/linebotjava/demo/Controller.java
@@ -6,6 +6,7 @@ import com.linecorp.bot.client.LineSignatureValidator;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,13 @@ public class Controller {
 
             eventsModel.getEvents().forEach((event)->{
                 //kode reply message disini
+                if(event instanceof MessageEvent){
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+                }
+
+                // replyText(replyToken, "Ini pesan balasan." );
             });
 
             return new ResponseEntity<>(HttpStatus.OK);
@@ -51,6 +59,26 @@ public class Controller {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private void reply(ReplyMessage replyMessage) {
+        try{
+            lineMessagingClient.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void replyText(String replyToken, String messageToUser) {
+        TextMessage textMessage = new TextMessage(messageToUser);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, textMessage);
+        reply(replyMessage);
+    }
+
+    private void replySticker(String replyToken, String packageId, String stickerId) {
+        StickerMessage stickerMessage = new StickerMessage(packageId, stickerId);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, stickerMessage);
+        reply(replyMessage);
     }
 
 }
